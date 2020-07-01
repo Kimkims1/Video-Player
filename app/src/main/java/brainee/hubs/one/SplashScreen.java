c7b625b1f4be3ab0229893a5960d3701d37116a6
Kimkims1 <kimanthikevin76@gmail.com>
2020-07-01 17:18:08 +0300

    """"

---
+++ app/src/main/java/brainee/hubs/one/SplashScreen.java
@@ -0,0 +1,83 @@
+package brainee.hubs.one;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import static android.os.Build.VERSION.SDK_INT;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static final int request_code = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        getSupportActionBar().hide();
+
+        checkPermission();
+    }
+
+    public void checkPermission() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                //Permission Granted
+            } else {
+
+                //ActivityCompat.requestPermissions(new String[Integer.parseInt(Manifest.permission.WRITE_EXTERNAL_STORAGE)], request_code);
+            }
+        } else {
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED && requestCode == request_code) {
+
+            //Go on
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        } else {
+
+            showDetails();
+        }
+    }
+
+    public void showDetails() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("WRITE STORAGE PERMISSION")
+                .setMessage("This permission is necessary to access videos in this device!")
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (SDK_INT >= Build.VERSION_CODES.M) {
+                            requestPermissions(new String[Integer.parseInt(Manifest.permission.WRITE_EXTERNAL_STORAGE)], request_code);
+                        }
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        finish();
+                    }
+                });
+    }
+}
